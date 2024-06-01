0bf847f3c08d452655c4b56f9d5a03d123995230
Krish <gujarathikrish@gmail.com>
2024-06-02 00:11:34 +0530

    second commit

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -5,7 +5,7 @@ import Stripe from "stripe";
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 // Placing user order for frontend
 const placeOrder = async (req, res) => {
-  const frontend_url = "http://localhost:3000";
+  const frontend_url = "https://foodapp1-frontend.onrender.com";
   try {
     const newOrder = new Order({
       userId: req.body.userId,
